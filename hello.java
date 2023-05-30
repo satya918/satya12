9940cbb07ca376863788a5bf9568974602a82da0
satya918 <satyamohanty918@gmail.com>
2023-05-30 11:12:58 +0530

    method updated

--- hello.java
+++ hello.java
@@ -1,5 +1,13 @@
 public class B{
 	public static void main(String [] args){
-		System.out.println("hello world ! ! !");
-}
+                     B b1=new B();
+		     b1.add();
+	}
+		public void add(){
+		 int a=9;
+		 int b=10;
+		 int c=a+b;
+
+		System.out.println(c);
+		}
 }
