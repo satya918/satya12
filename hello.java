21bab68e30da2dbf9516c787944d9024f2daf0d0
Satya <121515890+satya918@users.noreply.github.com>
2023-08-13 14:50:33 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,16 +1,5 @@
 public class B{
 	public static void main(String [] args){
-                     B b1=new B();
-		     b1.add();
-	}
-		public void add(){
-		 int a=9;
-		 int b=10;
-		 int c=a+b;
-			int b=10+4;
-			
-		System.out.println(bcfgh);
-		System.out.println(c);
-			System.out.println(c);
+                    System.out.println("Helloworld")
 		}
 }
